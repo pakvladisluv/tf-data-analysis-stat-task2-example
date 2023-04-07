20766dfc9e8e4075dc31e7d4098a83e8e15a8b4c
pakvladisluv <106927674+pakvladisluv@users.noreply.github.com>
2023-04-07 18:06:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,13 +12,20 @@ def solution(p: float, x: np.array) -> tuple:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = np.mean(x)
+    n = x.shape[0]
+    #loc = np.mean(x)
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    left = chi2.ppf(1 - alpha/2, df=len(x))
-    right = chi2.ppf(alpha/2, df=len(x))
-    y = 0
-    for i in range(len(x)):
-        y = y + x[i]**2
+    #left = chi2.ppf(1 - alpha/2, df=len(x))
+    #right = chi2.ppf(alpha/2, df=len(x))
+    #y = 0
+    #for i in range(len(x)):
+    #    y = y + x[i]**2
+    #left = norm.ppf(1 - alpha/2, df=len(x))
+    #right = norm.ppf(alpha/2, df=len(x))
+    left1 = chi2.ppf(1-alpha/2, 2*n)
+    right1 = chi2.ppf(alpha/2, 2*n)
+    left = np.sqrt(sum(x**2) / left1 * 35)
+    right = np.sqrt(sum(x**2) / right1 * 35)
     #return loc - scale * norm.ppf(1 - alpha / 2), \
     #       loc - scale * norm.ppf(alpha / 2)
-    return y / (35*left), y / (35*right)
+    return left, right
